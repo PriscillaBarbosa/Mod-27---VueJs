2d16e0059345c712e4bdb3ec812c5d0beb6ebcbc
Priscilla Barbosa <priscillabarbosa@outlook.com>
2024-08-01 19:01:13 -0300

    erro ao abrir o VueJs

---
+++ vue-project/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+const nome = "priscilla b"
+const meuObj = {
+  nome: "priscilla",
+  serieFavorita: "Game of Thrones"
+}
+
+function dizOla(nome) {
+  return `${nome} diz oi`;
+}
+
+const enderecoDaImagemDoDragao = "https://tm.ibxk.com.br/2024/07/16/16112748460102.jpg?ims=750x"
+const botaoEstaDesabilitado = false
+
+const gostaDoDragao = false
+
+</script>
+
+
+
+<template>
+  <h1>{{ dizOla("Paula") }}</h1>
+  <img v-show="gostaDoDragao" :src="enderecoDaImagemDoDragao">
+
+  <button :disabled="!botaoEstaDesabilitado">enviar mensagem</button>
+
+</template>
+
+<style scoped>
+
+</style>
